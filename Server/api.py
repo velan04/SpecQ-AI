e2380cf948a427957684aaace210f7f619e86b01
Velan V <velan.v@iamneo.ai>
2026-03-18 09:01:47 +0530

    url changed

--- Server/api.py
+++ Server/api.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://your-frontend-name.onrender.com"],
+    allow_origins=["http://localhost:5173", "https://specq-ai-jdl3.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
