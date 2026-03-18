4632c01bc0850904ac24a169fe182a62db9eba43
Velan V <velan.v@iamneo.ai>
2026-03-18 09:38:03 +0530

    Reload

--- Server/api.py
+++ Server/api.py
@@ -147,4 +147,4 @@ async def websocket_logs(ws: WebSocket):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=port)
